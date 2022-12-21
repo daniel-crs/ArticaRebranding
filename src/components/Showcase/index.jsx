7c42e3f964eb78886d09316d1623904d349d3878
Daniel <daniel.reis1911@gmail.com>
2022-12-21 18:06:04 -0300

    Foi feta a conditional render do button onde nao aparece mais em telas pequena, alem do update no css do componente para corrigir praticas ruis e o funcionamento errado em telas medianas.

--- src/components/Showcase/index.jsx
+++ src/components/Showcase/index.jsx
@@ -1,21 +1,53 @@
-import './showcase.css'
+import "./showcase.css";
+
+import Container from "react-bootstrap/Container";
+import { useEffect, useState } from "react";
 
 function Showcase() {
-    return(
-        <section>
-            <div className="container-fluid masthead">
-                <div id="container-showcase">
-                    <h1>Desenvolvimento</h1>
-                    <h1 className="color-h1-blue">Web e Mobile</h1>
-                    <p id="layout-info">
-                        Especializados nas tecnologias mais modernas para atender
-                        quais quer necessidades que você tenha.
-                    </p>
-                    <button type="button" className="btn-about-us" >Sobre nós</button>
-                </div>
-            </div>
-        </section>
-    )
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  return (
+    <Container fluid className="masthead">
+      <div>
+        <div id="container-showcase">
+          <h1>Desenvolvimento</h1>
+          <h1 className="color-h1-blue">Web e Mobile</h1>
+          <p id="layout-info">
+            Especializados nas tecnologias mais modernas para atender quais quer
+            necessidades que você tenha.
+          </p>
+          {(() => {
+            if (windowSize.innerWidth > 1023) {
+              return (
+                <button type="button" className="btn-about-us">
+                  Sobre nós
+                </button>
+              );
+            } else {
+              return null;
+            }
+          })()}
+        </div>
+      </div>
+    </Container>
+  );
+}
+
+function getWindowSize() {
+  const { innerWidth } = window;
+  return { innerWidth };
 }
 
 export default Showcase;
