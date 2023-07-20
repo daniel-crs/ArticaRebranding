0b75a9f50e6ece41cc8ad45056a8b9baf3c6b2d2
Daniel <daniel.reis1911@gmail.com>
2023-07-19 22:14:37 -0300

    Vinculei o content type responsolvel por armazenar dos dados do slider compont working ao frontend e está funcionando normalmente. alem disso, tambem vinculei os content type que armazena as informações e links da footer ao frontend.

--- src/components/Footer/ArticaInfo.jsx
+++ src/components/Footer/ArticaInfo.jsx
@@ -1,12 +1,31 @@
+import { useState, useEffect } from "react";
+
 function ArticaInfo() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const url = "http://localhost:1337/api/contact-infos";
+    fetch(url)
+      .then((res) => res.json())
+      .then((contact) => {
+        setData(contact.data);
+      });
+  }, []);
+
+  console.log(data);
+
   return (
     <div>
-      <h2 className="artica-footer-title">Artica</h2>
-      <div className="artica-footer-info">
-        <p>+55 (61) 996252273</p>
-        <p>artica-dev@gmail.com</p>
-        <p>Segunda a Sexta (8:00 - 18:00)</p>
-      </div>
+      {data.map((contact, i) => (
+        <div key={i}>
+          <h2 className="artica-footer-title">Artica</h2>
+          <div className="artica-footer-info">
+            <p>{contact.attributes.phoneNumber}</p>
+            <p>{contact.attributes.email}</p>
+            <p>{contact.attributes.expedient}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
